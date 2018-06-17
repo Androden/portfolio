b366705ad2e10e749bade81190d0d8f2ca670c30
Denis Andreev <andreevdenis43@gmail.com>
2018-06-17 22:29:35 +0300

    First Commit

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,7 +1,9 @@
 module.exports = {
   white: "#efeee9",
   beige: "#eeebdf",
+  lightBeige: "#f0efe9",
   green: "#16c2aa",
+  greenHover: "#009688",
   "text-color": "#455a64",
   grey: "#373e42",
   "light-grey": "#dfdcd5"
